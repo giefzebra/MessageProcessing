ccf3e85e84b9bfc7014ad132d982dcf7919bfd6c
Sam Rothwell <sam.rothwell@gmail.com>
2021-11-08 10:12:10 +0000

    New version

--- MessageProcessing.cs
+++ MessageProcessing.cs
@@ -11,26 +11,40 @@ namespace hso.messages
     public static class MessageProcessing
     {
         [FunctionName("MessageProcessing")]
-        public static void Run([TimerTrigger("0 */2 * * * *")] TimerInfo myTimer, ILogger log)
+        public static void Run([TimerTrigger("*/5 * * * * *")] TimerInfo myTimer, ILogger log)
         {
-            // Connection String
-            string connectionString = "DefaultEndpointsProtocol=https;AccountName=sjrrothwellqueuestorlrn;AccountKey=3WgQK1u5DWCTLs6Di2z+/k2hWT6hJGpdBgWzH49PiwZJVNqn+P+yyE4anp3KFm44YOI+JQTaSz809CMciJfysA==;EndpointSuffix=core.windows.net";
+            // Connection String & Queue to Read
+            string connectionString = Environment.GetEnvironmentVariable("WEBSITE_QUEUELOCATION");
+            string queueName = Environment.GetEnvironmentVariable("WEBSITE_QUEUENAME");
 
-            // Name of an existing queue we'll operate on
-            string queueName = "orderprocessing";
+            // Parameters END
 
-            // Get a reference to a queue 
+            // Setup Queue Reference
             QueueClient queue = new QueueClient(connectionString, queueName);
 
             // Get the next messages from the queue
-            foreach (QueueMessage message in queue.ReceiveMessages(maxMessages: 32).Value)
+            QueueProperties properties = queue.GetProperties();
+            int countmessages = properties.ApproximateMessagesCount;
+            int i = 1;
+
+            do
             {
-                // "Process" the message
-                log.LogInformation($"Message: {message.Body}");
-                // Let the service know we're finished with the message and
-                // it can be safely deleted.
-                queue.DeleteMessage(message.MessageId, message.PopReceipt);
-            }
+
+                foreach (QueueMessage message in queue.ReceiveMessages(maxMessages: 1).Value)
+                {
+
+                    // "Process" the message
+                    log.LogInformation($"Message: {message.Body}");
+                    // Let the service know we're finished with the message and
+                    // it can be safely deleted.
+                    queue.DeleteMessage(message.MessageId, message.PopReceipt);
+                    i++;
+                }
+
+            } while (i <= countmessages);
+
         }
+
     }
+
 }
